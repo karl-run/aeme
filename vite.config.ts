82dc5f92a05aae5cbd91e929e8b11f13f56a6e52
Karl J. Overå <k@rl.run>
2026-09-23 19:16:12 +0200

    chore: add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 import tanstackRouter from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     tanstackRouter({
       target: "react",
       autoCodeSplitting: true,
